2f338edcb6e08e1dd3f7d305302369c5717dbf29
RafaelD3v <rafaelcontato.ce@gmail.com>
2022-03-08 10:57:27 -0300

    Finished transaction list

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -100,13 +100,6 @@ export const Title = styled.Text`
   margin-bottom: 16px;
 `;
 
-/* export const TransactionList = styled(
-  FlatList as new () => FlatList<DataListProps>
-).attrs({
-  showsVerticalScrollIndicator: false,
-  contentContainerStyle: { paddingBottom: getBottomSpace() },
-})``; */
-
 export const TransactionList = styled(
   FlatList as new (
     props: FlatListProps<DataListProps>
